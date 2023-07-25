1e97dbdc6fdc5074453f4d638194f01132daf973
Jake Magill <jake.magill@redfin.com>
2023-07-25 10:25:18 -0700

    Fixing prompt. Now uses github standard prompt code

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,15 +1,19 @@
 # Formats the prompt to be:
 # user:currentDirectory (git branch)$ 
 
+# Find the source directory, handling symlinks
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+BASH_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+source $BASH_DIR/git-prompt.sh
+
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='\[\033[36m\]\u\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\[\033[0;35m\]$(__my_git_ps1)\[\033[m\]$ '
-
-# My functions
-__my_git_ps1 ()
-{
-  if [ "`type __git_ps1 2>/dev/null | grep function`" ]; then
-    __git_ps1 " (%s)"
-  fi
-}
+export GIT_PS1_SHOWUPSTREAM="auto"
+export PS1='\[\033[36m\]\u\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\[\033[0;35m\]$(__git_ps1)\[\033[m\]$ '
